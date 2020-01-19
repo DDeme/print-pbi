718a2ddc9b72b4182690366af492f8372f5ffa07
DDeme <daniel.demecko@gmail.com>
2020-01-19 19:09:35 +0100

    readFilesAsText

--- handleFileLoad.ts
+++ handleFileLoad.ts
@@ -1,29 +1,43 @@
-const readFile = (file, onLoadCallback) => {
-    var reader = new FileReader();
-    reader.onload = onLoadCallback;
-    reader.readAsText(file);
-}
 
 
-export const handleFileSelect = (evt) => {
-    const files = evt.target.files; // FileList object
-
-    // files is a FileList of File objects. List some properties.
+const readFilesAsText = (files: FileList): string[] => {
+  // files is a FileList of File objects. List some properties.
+    
+    const filesResults: string[] = []
     for (let i = 0, file; file = files[i]; i++) {
 
-      // Only process image files.
-      // if (!f.type.match('image.*')) {
+      // Only process csv, excel files.
+      // if (!file.type.match('image.*')) {
       //   continue;
       // }
+
+      //console.log(file.type)
 
       const reader = new FileReader();
 
       // Closure to capture the file information.
 
       reader.onload = () => {
-        console.log(reader.result);
+        if (typeof reader.result === 'string') {
+          filesResults.push(reader.result)
+        }
       };
+
+      reader.onerror = () => {
+        throw new Error(`Enable to read file ${file.name}`);
+      }
       // Read in the image file as a data URL.
       reader.readAsText(file);
     }
+
+    if (filesResults.length === 0) {
+      throw new Error(`No files selected`)
+    }
+
+    return filesResults
+}
+
+export const handleFileSelect = (evt): string[] => {
+    const files:FileList = evt.target.files; // FileList object
+    return readFilesAsText(files)
 }
